1808959842ad6f4cfe980fe98e8c2fac7859fe4e
Billy <billygun27@gmail.com>
2018-02-06 12:31:56 +0700

    make it better

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://iot.eclipse.org')
+ 
+client.on('connect', function () {
+  client.subscribe('presence')
+  client.publish('presence', 'Hello mqtt')
+})
+ 
+var msg
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+msg = message.toString()
+  // client.end()
+})
+
+const app = express();
+app.get('/hope' , (request,response) => {
+   
+    response.send(msg);
+    
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
