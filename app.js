cce58bd8ecf09d26f1f19cd70ff31f29ccc7ed52
Billy <billygun27@gmail.com>
2018-02-06 12:48:26 +0700

    new

--- app.js
+++ app.js
@@ -27,4 +27,7 @@ app.get('/hope' , (request,response) => {
     
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+//app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
+});
