e54151fec45b8d350a3c92e58c58fe46d1e006ce
Billy <billygun27@gmail.com>
2018-02-06 17:41:02 +0700

    pg

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://iot.eclipse.org')
+
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://ec2-23-21-162-90.compute-1.amazonaws.com:5432/dake2nd0sealua';
+
+const db = new pg.Client(connectionString);
+db.connect();
+
+
+db.query('INSERT INTO sensor (value) VALUES ($1::text as message)', ['100'], (err, res) => {
+ // console.log(err ? err.stack : res.rows[0].message) // Hello World!
+  db.end()
+})
+
  
 client.on('connect', function () {
   client.subscribe('presence')
@@ -35,3 +48,16 @@ var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('Our app is running on http://localhost:' + port);
 });
+
+
+/*
+const { Client } = require('pg')
+const client = new Client()
+
+client.connect()
+
+client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+  console.log(err ? err.stack : res.rows[0].message) // Hello World!
+  client.end()
+})
+*/ 
