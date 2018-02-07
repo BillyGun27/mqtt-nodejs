9d0f99309f7fa60abd75cc2b9b64900d9f939742
Billy <billygun27@gmail.com>
2018-02-07 13:10:48 +0700

    dc

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(request, response, next) {
-  res.send("hello");
+  response.send("hello");
 });
 
 module.exports = router;
